6a32d4dbcd56612fdf22950752b0538ea10d1d0b
masonromant2 <mason.romant2@gmail.com>
2024-02-20 19:42:07 -0500

    user skeleton

--- flasknotesapp/objects/user.py
+++ flasknotesapp/objects/user.py
@@ -1,11 +1,12 @@
+from PIL import Image
 class User:
     user_id: str
     username: str
     password: str
     email: str
-    #private group list
-    #public group list
-    #profile picture
+    profile_picture: Image
+    priv_groups: list
+    public_groups: list
 
     def __init__(self, user_id: str, username: str, password: str, email: str):
         self.user_id = user_id
