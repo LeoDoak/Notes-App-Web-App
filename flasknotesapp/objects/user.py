635485267eee11aa961f0c2fae539d4fecd5bfda
masonromant2 <mason.romant2@gmail.com>
2024-02-20 20:29:01 -0500

    added image

--- flasknotesapp/objects/user.py
+++ flasknotesapp/objects/user.py
@@ -9,10 +9,11 @@ class User:
     priv_groups: list
     public_groups: list
 
-    def __init__(self, user_id: str, username: str, password: str, email: str):
+    def __init__(self, user_id: str, username: str, password: str, email: str, profile_picture: Image):
         self.user_id = user_id
         self.username = username
         self.password = password
         self.email = email
+        self.profile_picture = profile_picture
 
     # sample list
