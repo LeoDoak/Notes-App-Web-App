a64ab3601b39413bf9891d5a908829a99f216703
Santosh <117950813+santoshbbs@users.noreply.github.com>
2024-02-20 21:46:06 -0500

    Created groups class

---
+++ flasknotesapp/groups.py
@@ -0,0 +1,16 @@
+class groups: 
+# A class representing Groups created by user"
+    group_id: int
+    group_name: str
+    isPrivate: bool
+def _init_(self, id: int, group_name: str, isPrivate: bool):
+    self.group_id = id
+    self.group_name: group_name
+    self.isPrivate: isPrivate
+
+
+# Sample list of groups
+
+
+
+]
