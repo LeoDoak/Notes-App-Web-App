bba8e384469bc44b6064cbc4eab656f716447db3
Leo Doak <125704724+LeoDoak@users.noreply.github.com>
2024-03-08 15:38:29 -0500

    Added error for invalid entry, linked other sites to login

--- flasknotesapp/server.py
+++ flasknotesapp/server.py
@@ -22,16 +22,23 @@ def login():
     get_password = request.form['password']
     connection = sqlite3.connect("databases/user.db")
     cursor =  connection.cursor()
-    cursor.execute("SELECT username, password FROM user where (username = ? and password = ?)",(get_name, get_password))
+    cursor.execute("SELECT username, password FROM user where (username = ? and password = ?)",(get_name.strip(), get_password.strip()))
     row = cursor.fetchall()
     connection.close()
     if len(row) == 1: 
-        return render_template('index.html') 
+        return render_template('homepage.html') 
     else:
         error_message = "Incorrect Username or Password!"
         return render_template("loginpage.html", msg = error_message) 
 
+@app.route('/register')
+def register(): 
+    return render_template("register.html")
+
+@app.route('/forgotpsd')
+def forgot_password():
+    return render_template("forgot_pswd.html")
+
 if __name__ == "__main__":
     serve(app, host = "0.0.0.0", port = 8000)
-
 login()
