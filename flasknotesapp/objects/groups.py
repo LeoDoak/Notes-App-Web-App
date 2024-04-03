107c7ca70d6ea07e410343f62f29a686c3f4fd15
Cole Thibault <colethibault@Coles-MacBook-Pro.local>
2024-04-03 15:11:31 -0400

    mypy errors maybe

--- flasknotesapp/objects/groups.py
+++ flasknotesapp/objects/groups.py
@@ -8,8 +8,8 @@ class Groups:
     def __init__(self, group_id: int, group_name: str, is_private: bool):
 
         self.group_id = group_id
-        self.group_name: group_name
-        self.is_private: is_private
+        self.group_name = group_name
+        self.is_private = is_private
 
 
 SAMPLE_GROUP = [
