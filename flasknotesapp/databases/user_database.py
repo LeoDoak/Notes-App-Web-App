4fb642719e944a3aa3d27270034b5934ffdb83bc
Cole Thibault <colethibault@Coles-MacBook-Pro.local>
2024-04-02 22:17:53 -0400

    Fixed a couple of flake8 issues

--- flasknotesapp/databases/user_database.py
+++ flasknotesapp/databases/user_database.py
@@ -1,21 +1,24 @@
-#https://www.ionos.com/digitalguide/websites/web-development/sqlite3-python/
+# https://www.ionos.com/digitalguide/websites/web-development/sqlite3-python/
 
 '''
-Run this file to create the database, checks the database to make sure that it already has admin access in it and has the names, 
-nobody should have to run this since the database should be created and stored within the user databse folder
+Run this file to create the database, checks the database to make sure that it
+already has admin access in it and has the names,
+nobody should have to run this since the database should be created and
+stored within the user databse folder
 '''
 
 import sqlite3
 
-def create_database(): 
-	connection = sqlite3.connect("user.db")
-	#check to see if database is created 
-	cursor =  connection.cursor()
-	#check for admin access 
-	cursor.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER, username TEXT, password TEXT,email TEXT)")
-	cursor.execute("SELECT username, password FROM user where user_id = 0 and email = 'admin@uncw.edu'")
-	row = cursor.fetchall()
-	#if no admin access found then it'll add one
+
+def create_database():
+    connection = sqlite3.connect("user.db")
+    #check to see if database is created 
+    cursor =  connection.cursor()
+    #check for admin access 
+    cursor.execute("CREATE TABLE IF NOT EXISTS user(user_id INTEGER, username TEXT, password TEXT,email TEXT)")
+    cursor.execute("SELECT username, password FROM user where user_id = 0 and email = 'admin@uncw.edu'")
+    row = cursor.fetchall()
+    #if no admin access found then it'll add one
 	if (len(row) == 0 ):
 		cursor.execute("INSERT INTO user VALUES(0, 'admin','1234','admin@uncw.edu')") #create admin access
 		#print("Admin access has been added to the database")
