b64e9f14d43d078087e9ba07163c248feccd3da1
Santosh <117950813+santoshbbs@users.noreply.github.com>
2024-04-07 18:25:51 -0400

    When you click on any of the displaying groups on sidebar it redirects to a new page with title of the group

--- flasknotesapp/server.py
+++ flasknotesapp/server.py
@@ -269,9 +269,8 @@ def group_page():
 
     return render_template("groups.html")
 
+
 @app.route('/group_details')
-
-
 @login_required
 def group_details_page():
     # Get the value of the 'title' query parameter
