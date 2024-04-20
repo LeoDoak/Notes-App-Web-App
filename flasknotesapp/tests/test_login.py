3b62c5738178eac8fb542eb7c5b031893551f923
Cole Thibault <colethibault@Coles-MacBook-Pro.local>
2024-04-20 19:16:25 -0400

    made folder for test files and outline of login file

---
+++ flasknotesapp/tests/test_login.py
@@ -0,0 +1,8 @@
+
+# This is for testing the login functionality with valid credentials.
+def test_login_valid():
+    return None
+
+# For testing login functionality with invalid credentials.
+def test_login_invalid():
+    return None
