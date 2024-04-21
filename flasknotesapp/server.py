0fb6f83f7890d93793f029104ec2a698515f23ba
masonromant2 <mason.romant2@gmail.com>
2024-04-21 00:56:31 -0400

    onedrive code shows up in new webpage #123

--- flasknotesapp/server.py
+++ flasknotesapp/server.py
@@ -308,6 +308,7 @@ def upload_page():
     """
     json_headers = request.cookies.get(session['username'])
     if json_headers is None:
+        print(json_headers)
         return render_template('homepage.html')
     headers = json.loads(json_headers)
     timeout = 60
@@ -446,6 +447,9 @@ def onedrive():
     app_id = '5e84b5a7-fd04-4398-a15f-377e3d85703e'
     scopes = ['Files.ReadWrite']
     # global ACCESS_TOKEN
+    # user_code = get_user_code(app_id, scopes, session)
+    # print(user_code)
+    # access_token = get_access_token(session)
     access_token = generate_access_token(app_id, scopes)
     headers = {
         'Authorization': 'Bearer ' + access_token['access_token']
