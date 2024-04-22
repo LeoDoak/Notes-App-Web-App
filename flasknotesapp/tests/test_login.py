befd9c57fc42672f157f5b398bf188415f34da24
Cole Thibault <colethibault@Coles-MacBook-Pro.local>
2024-04-21 21:08:06 -0400

    added in some testing for login

--- flasknotesapp/tests/test_login.py
+++ flasknotesapp/tests/test_login.py
@@ -3,12 +3,20 @@
 This file will test login functionality using pytest.
 '''
 
+from server import app
 
-def test_login_valid():
+def test_login_valid(monkeypatch):
     '''
     This will test login functionality with valid inputs.
     '''
-    return None
+    def mock_login():
+        return True
+    
+    monkeypatch.setattr("server.login", mock_login)
+    test_client = app.test_client()
+    response = test_client.post(
+        "/login", data={"username": "valid_username", "password": "valid_password"}, follow_redirects=True)
+    assert response.headers["Location"] == "http://localhost/homepage"
 
 
 def test_login_invalid():
